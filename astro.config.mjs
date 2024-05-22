3086d18f90df8d53ad539b0d799dda76addd4987
Emanuel Estrada Larios <emamex98@users.noreply.github.com>
2024-05-22 04:54:09 +0000

    ahahhashshds

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify/functions";
 
 import vercel from "@astrojs/vercel/serverless";
 
