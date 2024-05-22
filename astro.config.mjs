c908ac0982c8e9eb8fc6c5ca7670e38852e7dea1
Emanuel Estrada Larios <emamex98@users.noreply.github.com>
2024-05-22 05:05:56 +0000

    netlify2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://voto-por-la-igualdad.netlify.app/quienes-somos/",
+  site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
