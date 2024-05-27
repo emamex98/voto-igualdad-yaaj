1ea85aacd27d67b2349d457e1275cf1c31b014db
Emanuel Estrada Larios <emamex98@users.noreply.github.com>
2024-05-27 04:07:31 +0000

    Update canonical

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vxi-yaaj.netlify.app/",
+  site: "https://votoporlaigualdadmexico.org/",
   integrations: [tailwind(), mdx(), sitemap()],
 });
